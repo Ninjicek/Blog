612f0cffa4191920ea12013429ac7df47681ce53
Saša Klíž <147810212+Ninjicek@users.noreply.github.com>
2024-05-14 12:15:08 +0200

    Update header.php

--- layout/header.php
+++ layout/header.php
@@ -3,7 +3,7 @@
         <h1><a href="index.php">Blog</a></h1>
         <form method="get" action="index.php" class="form-inline">
             <input type="text" name="search" class="form-control mr-2">
-            <button type="submit" class="btn btn-primary"><span class="material-symbols-outlined">search</span></button>
+            <button type="submit" class="btn-primary"><span class="material-symbols-outlined">search</span></button>
         </form>
     </div>
 </header>
